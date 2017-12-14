7aea34b327974d456547deea967cf9604dee77ab
Daniel <dansolnik@gmail.com>
2017-12-14 15:00:09 -0600

    added DELETE /users/me/token route for logout and tests

--- server/server.js
+++ server/server.js
@@ -121,6 +121,11 @@ app.post('/users/login', (req, res) => {
     }).catch((e) => res.status(400).send());
 });
 
+app.delete('/users/me/token', authenticate, (req, res) => {
+    req.user.removeToken(req.token).then(() => res.status(200).send())
+      .catch((e) => res.status(400).send(e));
+});
+
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
 })
